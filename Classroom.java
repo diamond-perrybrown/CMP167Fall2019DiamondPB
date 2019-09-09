a3e85db06184452b044098f11777602f7cb0bd23
diamond-perrybrown <54720323+diamond-perrybrown@users.noreply.github.com>
2019-09-09 12:03:48 -0400

    Add files via upload

---
+++ Classroom.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Classroom {
+	public static void main(String[] args) {                  
+		
+		
+	Scanner input=new Scanner(System.in);
+	System.out.println("Enter 3 int values");
+		int num1=input.nextInt();
+		int num2=input.nextInt();
+		int num3=input.nextInt();
+		int result=num1+num2+num3;
+		System.out.println (result);
+		
+		
+		
+	}
+	
+}
