50b570d57a9fff8498b4cc4c9204e9b2f66a5804
diamond-perrybrown <54720323+diamond-perrybrown@users.noreply.github.com>
2019-10-02 12:35:57 -0400

    Add files via upload

---
+++ Week5pt2.java
@@ -0,0 +1,23 @@
+package week5;
+
+import java.util.Scanner;
+public class Week5pt2 {
+	public static void main(String[] args) {
+		int num1;
+		int num2;
+		int numTotal;
+		
+		Scanner num = new Scanner (System.in);
+		System.out.println("Enter Number");
+		num1 = num.nextInt();
+		System.out.println("Enter Second Number");
+		num2 = num.nextInt();
+		numTotal = num1 + num2;
+		System.out.println(numTotal);
+		
+		
+		
+		
+	}
+
+}
